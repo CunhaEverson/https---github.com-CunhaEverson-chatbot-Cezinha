2fd33cc0cf852fb780b7fde6f03c82fecb2c95e8
victor-luna <75651151+victor-luna@users.noreply.github.com>
2021-12-09 22:08:02 -0300

    Update chat.js

--- src/assets/js/chat.js
+++ src/assets/js/chat.js
@@ -1,71 +1,71 @@
 class InteractiveChatbox {
-  constructor(a, b, c) {
-    this.args = {
-      button: a,
-      chatbox: b,
-    };
-    this.icons = c;
-    this.state = false;
-  }
-
-  display() {
-    const { button, chatbox } = this.args;
-
-    button.addEventListener("click", () => this.toggleState(chatbox));
-  }
-
-  toggleState(chatbox) {
-    this.state = !this.state;
-    this.showOrHideChatBox(chatbox, this.args.button);
-  }
-
-  showOrHideChatBox(chatbox, button) {
-    if (this.state) {
-      chatbox.classList.add("chatbox--active");
-      this.toggleIcon(true, button);
-    } else if (!this.state) {
-      chatbox.classList.remove("chatbox--active");
-      this.toggleIcon(false, button);
+    constructor(a, b, c) {
+        this.args = {
+            button: a,
+            chatbox: b
+        }
+        this.icons = c;
+        this.state = false;
     }
-  }
 
-  toggleIcon(state, button) {
-    const { isClicked, isNotClicked } = this.icons;
-    let b = button.children[0].innerHTML;
+    display() {
+        const { button, chatbox } = this.args;
 
-    if (state) {
-      button.children[0].innerHTML = isClicked;
-    } else if (!state) {
-      button.children[0].innerHTML = isNotClicked;
+        button.addEventListener('click', () => this.toggleState(chatbox))
     }
-  }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+        this.showOrHideChatBox(chatbox, this.args.button);
+    }
+
+    showOrHideChatBox(chatbox, button) {
+        if (this.state) {
+            chatbox.classList.add('chatbox--active')
+            this.toggleIcon(true, button);
+        } else if (!this.state) {
+            chatbox.classList.remove('chatbox--active')
+            this.toggleIcon(false, button);
+        }
+    }
+
+    toggleIcon(state, button) {
+        const { isClicked, isNotClicked } = this.icons;
+        let b = button.children[0].innerHTML;
+
+        if (state) {
+            button.children[0].innerHTML = isClicked;
+        } else if (!state) {
+            button.children[0].innerHTML = isNotClicked;
+        }
+    }
 }
 
-class InteractiveButtons {
-  constructor(a, b) {
-    this.args = {
-      buttonOpt: a,
-      chatOpt: b,
-    };
-    this.state = false;
-  }
+function click_faq_btn(btn_click) {
+    const faq_message = document.createElement("DIV");
+    faq_message.setAttribute("class", "messages__item messages__item--visitor");
 
-  displayOpt() {
-    const { buttonOpt, chatOpt } = this.args;
+    faq_message.innerHTML = btn_click.innerHTML;
 
-    buttonOpt.addEventListener("click", () => this.toggleStateOpt(chatOpt));
-  }
+    document.getElementById("div_messages").appendChild(faq_message);
+}
 
-  toggleStateOpt(chatOpt) {
-    this.state = !this.state;
-    this.showOrHideChatBoxOpt(chatOpt, this.args.buttonOpt);
-  }
-
-  showOrHideChatBoxOpt(chatOpt) {
-    if (!this.state) {
-      chatOpt.classList.remove("is--hide");
-    } else if (this.state) {
-      chatOpt.classList.add("is--hide");
+var input = document.getElementById("input_chatbox");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("msg_button").click();
     }
-  }
+})
+
+
+function click_msg_btn() {
+    const message = document.createElement("DIV")
+    message.setAttribute("class", "messages__item messages__item--visitor");
+
+    message.innerHTML = document.getElementById("input_chatbox").value;
+
+    document.getElementById("div_messages").appendChild(message);
+
+    document.getElementById("input_chatbox").value = '';
 }
